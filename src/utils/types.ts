91457a7a6449737948c96227fc6a844e42242204
joYyHack <agalaktionov5490@gmail.com>
2023-03-23 00:13:47 +0200

    update types and constants

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,8 +1,14 @@
-import { FetchTokenResult } from "@wagmi/core";
+import { Address, FetchTokenResult } from "@wagmi/core";
 import { BigNumber } from "ethers";
+import { TokenType } from "./consts&enums";
 
+export type TokenInfo = {
+  sourceToken: Address;
+  tokenType: TokenType;
+};
 export type TokenData = Omit<FetchTokenResult, "totalSupply" | "address"> & {
   address: string;
+  tokenInfo: TokenInfo;
 };
 export type UserTokenData = TokenData & {
   userBalance: string;
@@ -10,6 +16,12 @@ export type UserTokenData = TokenData & {
 export type DepositStruct = {
   token: TokenData;
   amount: BigNumber;
+};
+export type ClaimStruct = DepositStruct & {
+  claimId: number;
+  sourceChainId: number;
+  targetChainId: number;
+  isClaimed: boolean;
 };
 export type ValidationResult = {
   isSuccess: boolean;
